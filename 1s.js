2524419036eb6313df1728b3af22dfef95048be4
bomjovnet-stack <bomjov.net@gmail.com>
2026-07-11 14:34:15 +0300

    Update 1s.js

--- 1s.js
+++ 1s.js
@@ -1,20 +1,21 @@
 (function() {
-    const originalFetch = window.fetch;
-
-    window.fetch = async function(...args) {
-        const url = args[0];
-        
-        if (typeof url === 'string' && url.includes('yumata.github.io') && url.includes('vast.js')) {
-            console.log('[Блокировщик] Запрос fetch к vast.js заблокирован!');
-            
-            // Возвращаем пустой фейковый ответ, чтобы плеер не завис, а просто подумал, что скрипт пустой
-            return new Response('', {
-                status: 200,
-                statusText: 'OK',
-                headers: { 'Content-Type': 'application/javascript' }
-            });
+    // 1. Блокировка <script src="...vast.js">
+    const originalAppendChild = Element.prototype.appendChild;
+    Element.prototype.appendChild = function(element) {
+        if (element && element.tagName === 'SCRIPT' && element.src && 
+            element.src.includes('vast.js')) {
+            console.log('[Блок] SCRIPT:', element.src);
+            return element; // Отмена добавления
         }
-        
+        return originalAppendChild.apply(this, arguments);
+    };
+
+    // 2. Блокировка fetch('...vast.js')
+    const originalFetch = window.fetch;
+    window.fetch = function(...args) {
+        if (args[0] && typeof args[0] === 'string' && args[0].includes('vast.js')) {
+            console.log('[Блок] FETCH:', args[0]);
+            return Promise.resolve(new Response(''));
+        }
         return originalFetch.apply(this, args);
     };
-})();

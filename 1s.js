611e2b1ac3bb394a7dc6c3dd80db57fee6f6fe89
bomjovnet-stack <bomjov.net@gmail.com>
2026-07-11 14:36:31 +0300

    Update 1s.js

--- 1s.js
+++ 1s.js
@@ -1,21 +1,36 @@
 (function() {
-    // 1. Блокировка <script src="...vast.js">
-    const originalAppendChild = Element.prototype.appendChild;
-    Element.prototype.appendChild = function(element) {
-        if (element && element.tagName === 'SCRIPT' && element.src && 
-            element.src.includes('vast.js')) {
-            console.log('[Блок] SCRIPT:', element.src);
-            return element; // Отмена добавления
-        }
-        return originalAppendChild.apply(this, arguments);
-    };
+    // Функция-инициализатор заглушки
+    function initMock() {
+        // Создаем фейковый класс-конструктор
+        const MockVASTPlayer = function() {
+            console.log('[Блокировщик] Создан пустой экземпляр VASTPlayer');
+            
+            // Заглушаем стандартные методы, которые плеер обычно вызывает после создания
+            this.load = function() { return Promise.resolve(); };
+            this.play = function() { console.log('[Блокировщик] Вызов VASTPlayer.play заблокирован'); return Promise.resolve(); };
+            this.pause = function() {};
+            this.stop = function() {};
+            this.on = function() { return this; };
+            this.once = function() { return this; };
+            this.off = function() { return this; };
+        };
 
-    // 2. Блокировка fetch('...vast.js')
-    const originalFetch = window.fetch;
-    window.fetch = function(...args) {
-        if (args[0] && typeof args[0] === 'string' && args[0].includes('vast.js')) {
-            console.log('[Блок] FETCH:', args[0]);
-            return Promise.resolve(new Response(''));
-        }
-        return originalFetch.apply(this, args);
-    };
+        // Заменяем оригинальный объект в глобальной видимости
+        Object.defineProperty(window, 'VASTPlayer', {
+            get: function() {
+                return MockVASTPlayer;
+            },
+            set: function(val) {
+                // Если оригинальный скрипт попытается перезаписать VASTPlayer,
+                // мы все равно оставляем нашу безопасную заглушку
+                console.log('[Блокировщик] Попытка регистрации оригинального VASTPlayer отклонена');
+            },
+            configurable: true
+        });
+        
+        console.log('[Блокировщик] Заглушка для VASTPlayer успешно установлена');
+    }
+
+    // Запускаем перехватчик немедленно
+    initMock();
+})();
